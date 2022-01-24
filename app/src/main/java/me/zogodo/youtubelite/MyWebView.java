9c7d0e299910aa120867231c4928a1349e723322
zogodo <742782908@qq.com>
2022-01-24 21:08:26 +0800

    up

--- app/src/main/java/me/zogodo/youtubelite/MyWebView.java
+++ app/src/main/java/me/zogodo/youtubelite/MyWebView.java
@@ -5,7 +5,6 @@ import android.os.Build;
 
 import android.util.AttributeSet;
 import android.view.*;
-import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 
 import java.util.Stack;
