5014212a47f04f591b3a6c4c431f54acbe3d290a
zogodo <742782908@qq.com>
2022-01-27 19:18:08 +0800

    up

--- app/src/main/java/me/zogodo/youtubelite/MyWebChromeClient.java
+++ app/src/main/java/me/zogodo/youtubelite/MyWebChromeClient.java
@@ -5,6 +5,7 @@ import android.content.pm.ActivityInfo;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.ConsoleMessage;
 import android.webkit.WebChromeClient;
@@ -45,12 +46,13 @@ public class MyWebChromeClient extends WebChromeClient
         MainActivity.me.setContentView(view);
         MainActivity.me.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-        WindowManager.LayoutParams attrs = MainActivity.me.getWindow().getAttributes();
+        Window win = MainActivity.me.getWindow();
+        WindowManager.LayoutParams attrs = win.getAttributes();
         attrs.flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
-        MainActivity.me.getWindow().setAttributes(attrs);
-        MainActivity.me.getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        //MainActivity.me.getWindow().getDecorView().setSystemUiVisibility(3846 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-        //MainActivity.me.getWindow().getDecorView().setSystemUiVisibility(
+        win.setAttributes(attrs);
+        win.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        //win.getDecorView().setSystemUiVisibility(3846 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        //win.getDecorView().setSystemUiVisibility(
         //        View.SYSTEM_UI_FLAG_LOW_PROFILE
         //        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
         //        | View.SYSTEM_UI_FLAG_FULLSCREEN
@@ -63,10 +65,11 @@ public class MyWebChromeClient extends WebChromeClient
         MainActivity.me.setContentView(MyWebView.webview_stack.peek());
         MainActivity.me.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
 
-        WindowManager.LayoutParams attrs = MainActivity.me.getWindow().getAttributes();
+        Window win = MainActivity.me.getWindow();
+        WindowManager.LayoutParams attrs = win.getAttributes();
         attrs.flags &= (~WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        MainActivity.me.getWindow().setAttributes(attrs);
-        MainActivity.me.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        win.setAttributes(attrs);
+        win.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
         super.onHideCustomView();
     }
