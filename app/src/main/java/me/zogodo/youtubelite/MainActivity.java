1fed62dcf7f3041901ce5e26c5b0dc8abc2e3415
zogodo <742782908@qq.com>
2022-01-31 20:27:30 +0800

    原来这些都不需要...

--- app/src/main/java/me/zogodo/youtubelite/MainActivity.java
+++ app/src/main/java/me/zogodo/youtubelite/MainActivity.java
@@ -1,15 +1,10 @@
 package me.zogodo.youtubelite;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import org.adblockplus.libadblockplus.android.AdblockEngine;
-import org.adblockplus.libadblockplus.android.settings.AdblockHelper;
-import org.adblockplus.libadblockplus.android.webview.AdblockWebView;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -25,33 +20,8 @@ public class MainActivity extends AppCompatActivity
         getSupportActionBar().hide();
         MainActivity.me = this;
 
-        String basePath = getDir(AdblockEngine.BASE_PATH_DIRECTORY, Context.MODE_PRIVATE).getAbsolutePath();
-        AdblockHelper
-                .get()
-                .init(this, basePath, AdblockHelper.PREFERENCE_NAME)
-                .preloadSubscriptions(R.raw.easylist_minified, R.raw.exceptionrules_minimal);
-                //.setDisabledByDefault();
-        AdblockHelper.get().getProvider().retain(false);
-        AdblockHelper.get().getProvider().waitForReady();
-        final AdblockEngine adblockEngine = AdblockHelper.get().getProvider().getEngine();
-        adblockEngine.setEnabled(true);
-
-        /*
-        AdblockSettingsStorage storage = AdblockHelper.get().getStorage();
-        AdblockSettings settings = storage.load();
-        if (settings == null) // not yet saved
-        {
-            settings = AdblockSettingsStorage.getDefaultSettings(this); // default
-        }
-        settings.setAdblockEnabled(true);
-        storage.save(settings);
-        */
-
-        indexUrl = "https://zogodo.github.io";
-        indexUrl = "https://www.openguet.cn/lab/views/login.jsp";
-        indexUrl = "http://nsfwyoutube.com/";
-        webView = new AdblockWebView(this);
-        this.setContentView(webView);
+        //indexUrl = "http://nsfwyoutube.com/";
+        webView = new MyWebView();
         webView.loadUrl(indexUrl);
     }
 
