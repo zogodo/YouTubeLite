30ed4cd92fd3c02339d030c7355fc2b600594384
zogodo <742782908@qq.com>
2022-02-12 12:47:34 +0800

    自动取消静音

--- app/src/main/res/raw/myjs.js
+++ app/src/main/res/raw/myjs.js
@@ -1,6 +1,14 @@
-window.addEventListener('scroll', function () {
-    var links = document.querySelectorAll('a:not([target=_blank])');
+function ToBlank() {
+    var links = document.querySelectorAll("a:not([target=_blank])");
     for (var i = 0; i < links.length; i++) {
-        links[i].target = '_blank';
+        links[i].target = "_blank";
     }
-});
+};
+window.addEventListener("scroll", ToBlank);
+ToBlank();
+var but = document.getElementsByClassName("ytp-unmute");
+console.log("but:");
+console.log(but);
+if (but.length > 0) {
+    but[0].click();
+}
