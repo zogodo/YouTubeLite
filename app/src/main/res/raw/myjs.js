0f7db6ad33d5a58d1959710d833f4332ebc951c8
zogodo <742782908@qq.com>
2022-02-12 21:07:10 +0800

    默认720P

--- app/src/main/res/raw/myjs.js
+++ app/src/main/res/raw/myjs.js
@@ -6,9 +6,21 @@ function ToBlank() {
 };
 window.addEventListener("scroll", ToBlank);
 ToBlank();
-var but = document.getElementsByClassName("ytp-unmute");
-console.log("but:");
-console.log(but);
-if (but.length > 0) {
-    but[0].click();
+
+function ClickBut(query) {
+    var but = document.querySelector(query);
+    console.log(query);
+    console.log(but);
+    if (but != null) {
+        but.click();
+    }
+}
+ClickBut(".ytp-unmute");  /*取消静音*/
+
+const QUALITIES =  ['auto', 'tiny', 'small', 'medium', 'large', 'hd720', 'hd1080', 'hd1440', 'hd2160', 'hd2880', 'highres'];
+var movie_player = document.getElementById("movie_player");
+console.log(movie_player.getPlaybackQuality());
+if (movie_player.zz_set == null) {
+    movie_player.setPlaybackQualityRange("hd720");
+    movie_player.zz_set = true;
 }
