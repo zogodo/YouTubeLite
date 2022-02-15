de0f7fd5620852c0c7bd5745ad05a94488c945c0
zogodo <742782908@qq.com>
2022-02-15 20:13:57 +0800

    删除多余

--- app/src/main/java/me/zogodo/youtubelite/MainActivity.java
+++ app/src/main/java/me/zogodo/youtubelite/MainActivity.java
@@ -6,21 +6,14 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.media.MediaMetadata;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.media.MediaDescriptionCompat;
-import android.support.v4.media.MediaMetadataCompat;
-import android.support.v4.media.session.MediaControllerCompat;
-import android.support.v4.media.session.MediaSessionCompat;
-import android.support.v4.media.session.PlaybackStateCompat;
 import android.webkit.WebView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
-import androidx.media.session.MediaButtonReceiver;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -56,6 +49,8 @@ public class MainActivity extends AppCompatActivity
     {
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(
                 this, "MyChannelId");
+        Intent ii = new Intent(this.getApplicationContext(), MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, ii, 0);
         NotificationCompat.Builder mBuilder = notificationBuilder.setOngoing(true)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("YouTube is playing in the background")
@@ -66,84 +61,7 @@ public class MainActivity extends AppCompatActivity
                 // Add a pause button
                 .addAction(new NotificationCompat.Action(
                         R.drawable.ic_launcher_foreground, "Stop Play",
-                        MediaButtonReceiver.buildMediaButtonPendingIntent(this,
-                                PlaybackStateCompat.ACTION_PLAY_PAUSE)));
-
-        NotificationManager mNotificationManager =
-                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        // === Removed some obsoletes
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-        {
-            String channelId = "Your_channel_id";
-            NotificationChannel channel = new NotificationChannel(
-                    channelId,
-                    "Channel human readable title",
-                    NotificationManager.IMPORTANCE_HIGH);
-            mNotificationManager.createNotificationChannel(channel);
-            mBuilder.setChannelId(channelId);
-        }
-
-        mNotificationManager.notify(0, mBuilder.build());
-    }
-
-    private void MyNotify1()
-    {
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this.getApplicationContext(), "notify_001");
-        Intent ii = new Intent(this.getApplicationContext(), MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, ii, 0);
-
-        NotificationCompat.BigTextStyle bigText = new NotificationCompat.BigTextStyle();
-        bigText.bigText("android verseurl verseurl verseurl verseurl verseurl verseurl verseurl verseurl verseurl verseurl verseurl verseurl");
-        bigText.setBigContentTitle("Today's Bible Verse");
-        bigText.setSummaryText("Text in detail");
-
-        mBuilder.setContentIntent(pendingIntent);
-        //mBuilder.setSmallIcon(R.mipmap.ic_launcher);
-        mBuilder.setSmallIcon(R.drawable.ic_launcher_foreground);
-        mBuilder.setContentTitle("Your Title");
-        mBuilder.setContentText("Your text");
-        mBuilder.setPriority(Notification.PRIORITY_MAX);
-        mBuilder.setStyle(bigText);
-
-        NotificationManager mNotificationManager =
-                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        // === Removed some obsoletes
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-        {
-            String channelId = "Your_channel_id";
-            NotificationChannel channel = new NotificationChannel(
-                    channelId,
-                    "Channel human readable title",
-                    NotificationManager.IMPORTANCE_HIGH);
-            mNotificationManager.createNotificationChannel(channel);
-            mBuilder.setChannelId(channelId);
-        }
-
-        mNotificationManager.notify(0, mBuilder.build());
-    }
-
-    private void MyNotify()
-    {
-        // Create a media session. NotificationCompat.MediaStyle
-        // PlayerService is your own Service or Activity responsible for media playback.
-        MediaSessionCompat mediaSession = new MediaSessionCompat(this, "PlayerService");
-
-        // Create a MediaStyle object and supply your media session token to it.
-        androidx.media.app.NotificationCompat.MediaStyle mediaStyle = new androidx.media.app.NotificationCompat.MediaStyle().setMediaSession(mediaSession.getSessionToken());
-
-        // Specify any actions which your users can perform, such as pausing and skipping to the next track.
-        NotificationCompat.Action action = new NotificationCompat.Action(R.drawable.ic_launcher_foreground, "TT", null);
-
-        //Create a Notification which is styled by your MediaStyle object.
-        //This connects your media session to the media controls.
-        //Don't forget to include a small icon.
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, "zz1")
-                .setStyle(mediaStyle)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .addAction(action);
+                        pendingIntent));
 
         NotificationManager mNotificationManager =
                 (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
--- app/src/main/java/me/zogodo/youtubelite/MySessionCallback.java
+++
@@ -1,7 +0,0 @@
-package me.zogodo.youtubelite;
-
-import android.support.v4.media.session.MediaSessionCompat;
-
-public class MySessionCallback extends MediaSessionCompat.Callback
-{
-}
